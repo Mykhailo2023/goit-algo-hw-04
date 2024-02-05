6722ebee78cc389792687df93fd4ca3e0730f17e
Mykhailo2023 <152901832+Mykhailo2023@users.noreply.github.com>
2024-02-05 18:55:53 +0200

    a

--- DZ4_1.py
+++ DZ4_1.py
@@ -1,36 +1,26 @@
-
-def total_salary():
-
-     # Функція total_salary(path) має приймати один аргумент - шлях до текстового файлу (path).
-    path = 'salary_file.txt'
-# Робимо виключення якщо файл не існує
+def total_salary(path):
+    # якщо файла не існує
     try:
-        # відкриваємо файл на читання (не забуваємо про кодування)
-
+        # відкриваємо файл на читання
         with open(path, 'r', encoding='utf-8') as file:
-
-            # зчитуємо дані з файлу і розбиваємо  по комі
+            # розбиваємо полінійно, роздільник кома
             data = [tuple(line.strip().split(',')) for line in file]
-
-        # якщо нічого не знайшло 
+        # якщо файл пустий
         if not data:
             print("No data available.")
             return None, None
         
-        # математичні дії
         salaries = [float(row[1]) for row in data]
         total = sum(salaries)
         average = total / len(salaries)
-
-        # виводимо результат
-        print(f"Загальна сума заробітної плати: {total}, Середня заробітна плата: {average}")
-        return total, average        
+        
+        return total, average
 
     except FileNotFoundError:
-        print(f"File not found: {file_path}")
+        print(f"File not found: {path}")
         return None, None
 
 # Виклик функції
-total_salary()
-
-# я розумії що забагато коментарів, але поки мушу так робити щоб запамятати
+path = 'salary_file.txt'
+total, average = total_salary(path)
+print("Загальна сума заробітної плати:", total, ", Середня заробітна плата:", average)
--- DZ4_2.py
+++ DZ4_2.py
@@ -1,19 +1,31 @@
-
-def get_cats_info(file_path):
-    cats_dict = {}
+def get_cats_info(path):
+    cats_list = []
     try:
-        with open(file_path, 'r') as file:
-            for line in file:
-                # Розділяємо рядок за комою та отримуємо id, ім'я кота та рік
-                cat_info = line.strip().split(',')
-                if len(cat_info) == 3:
-                    id, name, year = cat_info
-                    cats_dict[id] = {'name': name, 'year': year} # тут можна виставляти поля 
-    except FileNotFoundError:
-        print(f"File not found: {file_path}")
-    return cats_dict
+        
+        with open(path, 'r', encoding='utf-8') as file:
+            lines = file.readlines()
 
-# Приклад виклику функції для створення словника
-file_path = 'cats_file.txt'
-cats_dict = get_cats_info(file_path)
-print(cats_dict)
+            for line in lines:
+                # Розділяємо рядок на ідентифікатор, ім'я та вік кота
+                cat_info = line.strip().split(',')
+                
+                # Формуємо словник для кота
+                cat_dict = {
+                    "id": cat_info[0],
+                    "name": cat_info[1],
+                    "age": int(cat_info[2])
+                }
+                
+                # Додаємо словник до списку
+                cats_list.append(cat_dict)
+
+        return cats_list
+
+    except FileNotFoundError:
+        print(f"Файл {path} не знайдено.")
+        return None
+
+# Приклад використання
+path = 'cats_file.txt'
+cats_info = get_cats_info(path)
+print(cats_info)
